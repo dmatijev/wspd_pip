5dc5edddbc5b185140212150f90d0920ba74c999
Domagoj Matijević <domagoj@mathos.hr>
2023-09-04 11:44:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
